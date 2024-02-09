4dfb152ba47ea710aa1cdfa8175ce6ae8ba9c9f8
anitta-mariya <anittamjoseph24@gmail.com>
2024-02-09 11:54:48 +0530

    mail

---
+++ model/user.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose")
+
+const usermodelSchema= mongoose.Schema(
+    {
+        name:String,
+        age:String,
+        address:String,
+        phonenumber:String,
+        pincode:String,
+        emailid:String,
+        password:String
+    }
+)
+
+module.exports=mongoose.model("blog",usermodelSchema)
+
